8ff21e3e99e47a2951c498ceffabe467a4b0b36c
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-01-08 18:01:15 -0800

    started BST implementation, can't get it working

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -6,14 +6,37 @@ class BinarySearchTree(BinaryTree):
     Following the rules of a BST, adds the value to the tree in the correct location
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, value=None):
+        super().__init__()
+        self.value = value
+        self.left = None
+        self.right = None
 
-    def contains(self):
-        # method body here
-        pass
+    def add(self, value):
+        if value == self.value:
+            return print("node already exists")
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.add(value)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.add(value)
 
-    def add(self):
-        # method body here
-        pass
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
