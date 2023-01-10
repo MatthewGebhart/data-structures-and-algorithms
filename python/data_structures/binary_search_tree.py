26d26e58489cdac4b825b4788eaee704753e1719
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-01-09 18:51:10 -0800

    finished BST functions CC 15 complete

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,42 +1,62 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
     """
-    Following the rules of a BST, adds the value to the tree in the correct location
+ Subclass of BinaryTree
     """
-
-    def __init__(self, value=None):
-        super().__init__()
-        self.value = value
-        self.left = None
-        self.right = None
-
     def add(self, value):
-        if value == self.value:
-            return print("node already exists")
-        if value < self.value:
-            if self.left is None:
-                self.left = BinarySearchTree(value)
+        """
+        Following the rules of a BST, adds the value to the tree in the correct location
+        """
+        # check for empty BST
+        if self.root is None:
+            self.root = Node(value)
+            return
+
+        # start pointer at root
+        current = self.root
+
+        while current:
+            # if value == current.value:
+            #     return print("node already exists")
+
+            # if number of the added node is less than current node, check left
+            if value < current.value:
+                # add node only if there's space
+                if current.left is None:
+                    current.left = Node(value)
+                    return
+
+                # if there is no space, go left
+                current = current.left
+
+            # if number of the added number is greater than current node, go right
             else:
-                self.left.add(value)
-        else:
-            if self.right is None:
-                self.right = BinarySearchTree(value)
-            else:
-                self.right.add(value)
+                # add node only if there is space
+                if current.right is None:
+                    current.right = Node(value)
+                    return
+
+                current = current.right
 
     def contains(self, target):
-        if target == self.value:
-            return True
-        elif target < self.value:
-            if self.left is None:
-                return False
-            else:
-                return self.left.contains(target)
-        else:
-            if self.right is None:
-                return False
-            else:
-                return self.right.contains(target)
+        """
+        searches through the BST looking for target and returns TRUE if it is present
+        """
+        # start pointer at root
+        current = self.root
+
+        while current:
+            #check if root is target
+            if target == current.value:
+                return True
+
+            if target < current.value:
+                current = current.left
+
+            if target > current.value:
+                current = current.right
+
+        return False
 
---
+++ python/docs/trees/README.md
@@ -0,0 +1,11 @@
+# Trees
+<!-- Short summary or background information -->
+
+## Challenge
+<!-- Description of the challenge -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+<!-- Description of each method publicly available in each of your trees -->
