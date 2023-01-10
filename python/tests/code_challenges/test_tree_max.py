0875a3570a80d93be44222dd8834f7863b1d555e
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-01-09 22:54:27 -0800

    CC 16 finished and passing

--- python/tests/code_challenges/test_tree_max.py
+++ python/tests/code_challenges/test_tree_max.py
@@ -2,7 +2,7 @@ import pytest
 from data_structures.binary_tree import BinaryTree, Node
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_max_val():
     tree = BinaryTree()
     tree.root = Node(10)
